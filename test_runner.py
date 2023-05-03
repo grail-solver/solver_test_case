8b0d109bd2886badcd5af91eb4c105f6d0979f14
Généreux AKOTENOU <genereux.akotenou@gmail.com>
2023-05-03 22:13:13 +0100

    Add problem

--- test_runner.py
+++ test_runner.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import os
+import json
+from xtractor.core.helper import grail_extractor
 
 
-# Press the green button in the gutter to run the script.
+def run_test():
+    test_folder_path = 'case'
+    for folder_name in os.listdir(test_folder_path):
+        folder_path = os.path.join(test_folder_path, folder_name)
+        if os.path.isdir(folder_path):
+            # For each problem file
+            for file_name in os.listdir(folder_path):
+                if file_name.endswith('.txt'):
+                    file_path = os.path.join(folder_path, file_name)
+                    with open(file_path, 'r') as file:
+                        text = file.read()
+
+                    # Extract relevant data from the text
+                    result = grail_extractor(text)
+
+                    # Save text output in file
+                    result_file_path = os.path.join(folder_path, 'result.json')
+                    with open(result_file_path, 'w') as result_file:
+                        json.dump(result, result_file)
+
+
+def run_specific_test(folder: str):
+    folder_path = os.path.join('case', folder)
+    if os.path.isdir(folder_path):
+            # For each problem file
+            for file_name in os.listdir(folder_path):
+                if file_name.endswith('problem.txt'):
+                    file_path = os.path.join(folder_path, file_name)
+                    with open(file_path, 'r') as file:
+                        text = file.read()
+
+                    # Extract relevant data from the text
+                    print(folder, ':Launch request')
+                    result = grail_extractor(text)
+                    print(folder, ':Launch done')
+
+                    print("TEST: SEE OUTPUT IN RESULT.TXT FILE")
+
+                    # Save text output in file
+                    result_file_path = os.path.join(folder_path, 'result.json')
+                    with open(result_file_path, 'w') as result_file:
+                        json.dump(result, result_file)
+
+#run_specific_test('case_38_sudoku')
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    run_specific_test('case_38_sudoku')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
